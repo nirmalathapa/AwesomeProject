549beeaeb2556e3b0b10325d3910055809c494d7
nirmala <nirmala@surf.net.np>
2018-08-19 16:22:07 +0200

    add heading, timer, buttons

--- App.js
+++ App.js
@@ -1,23 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
 
-export default class App extends React.Component {
+class Timer extends Component {
+  _onPressButton() {
+    ("this is press");
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View>
+        <Text style={styles.timer}>{this.props.time}</Text>
+
+        <View style={{ backgroundColor: "green", marginBottom: 10, borderRadius:10, }}>
+          <Button onPress={this._onPressButton} title="Start" color="white" />
+        </View>
+
+        <View style={{ backgroundColor: "red", marginBottom: 10, borderRadius:10 }}>
+          <Button onPress={this._onPressButton} title={"Stop"} color="white" />
+        </View>
+
+        <View style={{ backgroundColor: "blue", borderRadius:10 }}>
+          <Button onPress={this._onPressButton} title={"Reset"} color="white" />
+        </View>
+      </View>
+    );
+  }
+}
+
+export default class TimerChange extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center"
+        }}
+      >
+        <Text style={styles.heading}>Pomodoro Timer</Text>
+        <Timer time="25:00" />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  heading: {
+    fontWeight: "bold",
+    fontSize: 30,
+    color: "tomato"
   },
+  timer: {
+    fontSize: 40,
+    paddingTop: 20
+  }
 });
